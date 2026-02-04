45e25a49fff81b00fd3d1909c740992dbf26358a
Kei <maxwell.kei.farouk@gmail.com>
2026-02-04 17:18:00 +0200

    Refactor app initialization and update API endpoint names for clarity

--- app-module.js
+++ app-module.js
@@ -1,24 +1,38 @@
 const express = require("express");
 
-app = express();
+const app = express();
 
-// application level setting
-app.set("view engine", "ejs");
+// // application level setting
+// app.set("view engine", "ejs");
 
-// routing
-app.get("/", (req, res) => {
-  res.send("Home page!");
-});
+app.use(express.text());
+app.use(express.json());
+
+// // routing
+// app.get("/", (req, res) => {
+//   res.send("Home page!");
+// });
 
 const messages = [];
 
-app.post("/api/data", (req, res) => {
+app.post("/api/senddata", (req, res) => {
   messages.push(req.body);
+  res.send("Message received!");
+});
 
+app.get("/api/printdata", (req, res) => {
+  console.log(messages);
+  res.send(messages.join("\n"));
+});
+
+const jsonMessages = [];
+
+app.post("/api/sendjson", (req, res) => {
+  jsonMessages.push(req.body);
   res.json({ success: true });
 });
 
-app.get("/api/printdata", (req, res) => {});
+app.get("/api/printjson", (req, res) => {});
 
 port = 3000;
 app.listen(3000, () => {
